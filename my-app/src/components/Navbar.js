f102b9ded7151d303e4be6c8a39c7c5fd2ad8b59
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-04-30 08:22:47 +0100

    First commit before push

---
+++ my-app/src/components/Navbar.js
@@ -0,0 +1,12 @@
+// import myimage from "../images/a1.png";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <img src="./images/a1.png" alt="" />
+      <h1>This is the Navbar, bitch</h1>
+    </div>
+  )
+}
+
+export default Navbar;
