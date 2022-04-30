d8614cdb334284c2b97e1ac428017272ad32216d
Abdulmujeeb Lawal <lawalmujeeb78@gmail.com>
2022-04-30 12:29:53 +0100

    Added varying data

--- my-app/src/data.js
+++ my-app/src/data.js
@@ -4,7 +4,7 @@ const data = [
     img: "a1.png",
     country: "JAPAN",
     location: "Mount Fuji",
-    date: "12 Jan, 2021 - 24 Jan, 2021",
+    date: "21 Jan, 2021 - 32 Jan, 2021",
     description:
       "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meter (12,380 feet). Mount Fuji is the single most popular tourist site in  Japan, for both Japanese and foreign tourists.",
   },
@@ -12,53 +12,42 @@ const data = [
   
   {
     id:2,
-    img:"a1.png",
-    country: "JAPAN",
-    location: "Mount Fuji",
-    date: "12 Jan, 2021 - 24 Jan, 2021",
+    img:"a2.png",
+    country: "NIGERIA",
+    location: "Yankari",
+    date: "12 Aug, 2021 - 14 Jan, 2022",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meter (12,380 feet). Mount Fuji is the single most popular tourist site in  Japan, for both Japanese and foreign tourists.",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas illo, fuga officiis perferendis repellendus architecto aliquam. Culpa, soluta deleniti, omnis error nemo optio atque corrupti recusandae ducimus voluptates, aspernatur id"
   },
 
   {
     id:3,
-    img: "a1.png",
-    country: "JAPAN",
-    location: "Mount Fuji",
-    date: "12 Jan, 2021 - 24 Jan, 2021",
+    img: "a3.png",
+    country: "ENGLAND",
+    location: "Park Central",
+    date: "31 Jan, 2021 - 24 Jan, 2023",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meter (12,380 feet). Mount Fuji is the single most popular tourist site in  Japan, for both Japanese and foreign tourists.",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas illo, fuga officiis perferendis repellendus architecto aliquam. Culpa, soluta deleniti, omnis error nemo optio atque corrupti recusandae ducimus voluptates, aspernatur id"
   },
 
   {
     id:4,
-    img: "a1.png",
-    country: "JAPAN",
-    location: "Mount Fuji",
+    img: "a4.png",
+    country: "AMERICA",
+    location: "Times Square",
     date: "12 Jan, 2021 - 24 Jan, 2021",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meter (12,380 feet). Mount Fuji is the single most popular tourist site in  Japan, for both Japanese and foreign tourists.",
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas illo, fuga officiis perferendis repellendus architecto aliquam. Culpa, soluta deleniti, omnis error nemo optio atque corrupti recusandae ducimus voluptates, aspernatur id"
   },
 
   {
     id:5,
     img: "a1.png",
-    country: "JAPAN",
-    location: "Mount Fuji",
-    date: "12 Jan, 2021 - 24 Jan, 2021",
-    description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meter (12,380 feet). Mount Fuji is the single most popular tourist site in  Japan, for both Japanese and foreign tourists.",
-  },
-
-  {
-    id:6,
-    img: "a1.png",
-    country: "JAPAN",
-    location: "Mount Fuji",
-    date: "12 Jan, 2021 - 24 Jan, 2021",
-    description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meter (12,380 feet). Mount Fuji is the single most popular tourist site in  Japan, for both Japanese and foreign tourists.",
-  },
+    country: "FRANCE",
+    location: "Eiffel Tower",
+    date: "14 Jan, 2024 - 24 Jan, 2024",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas illo, fuga officiis perferendis repellendus architecto aliquam. Culpa, soluta deleniti, omnis error nemo optio atque corrupti recusandae ducimus voluptates, aspernatur id"
+}
 
 
 ]
